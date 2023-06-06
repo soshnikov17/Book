401173bd4d828af0bd5b1ff6258ddf888dcf403c
Kirill Soshnikov <soshnikov17@gmail.com>
2023-06-06 10:22:17 +0300

    Добавил строку в feature-c

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,5 +5,6 @@ public class App {
     public void openShopInfo() {
         System.out.println("Открытие магазина через 2 месяца");
         System.out.println("Волью в мастер");
+        System.out.println("Добавил строку");
     }
 }
