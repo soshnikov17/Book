7f078c104ae74d9eafd0883ab4b4119ece60dccd
Kirill Soshnikov <soshnikov17@gmail.com>
2023-06-06 09:46:52 +0300

    Сherry-pick из feature-a в feature-c

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,7 @@
 package org.example;
 
 public class App {
-
-    public void openShopInfo() {
-        System.out.println("Открытие магазина через 2 месяца");
-        System.out.println("Волью в мастер");
-        System.out.println("Добавил строку");
-        System.out.println("Добавил еще строку");
+    public void openShopInfo(){
+        System.out.println("Открытие магазина ожидается через 4 дня");
     }
 }
