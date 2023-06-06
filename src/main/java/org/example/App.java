9bfafef50e428e4b409c7e526ec513433c8438b2
Kirill Soshnikov <soshnikov17@gmail.com>
2023-06-06 10:22:58 +0300

    Убрал строку в feature-c

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,5 @@ public class App {
     public void openShopInfo() {
         System.out.println("Открытие магазина через 2 месяца");
         System.out.println("Волью в мастер");
-        System.out.println("Добавил строку");
     }
 }
