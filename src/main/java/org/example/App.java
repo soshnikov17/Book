85dd461abb07f03b65cf28859ea46f71a27b6e60
Kirill Soshnikov <soshnikov17@gmail.com>
2023-06-06 10:09:56 +0300

    Волью в мастер

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,6 @@ package org.example;
 public class App {
     public void openShopInfo() {
         System.out.println("Открытие магазина через 2 месяца");
+        System.out.println("Волью в мастер");
     }
 }
