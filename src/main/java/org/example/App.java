da05eb657d141d61bd55dabe2de8a5977226e8b3
Kirill Soshnikov <soshnikov17@gmail.com>
2023-06-06 10:23:36 +0300

    Добавил две строки в feature-c

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,5 +5,7 @@ public class App {
     public void openShopInfo() {
         System.out.println("Открытие магазина через 2 месяца");
         System.out.println("Волью в мастер");
+        System.out.println("Добавил строку");
+        System.out.println("Добавил еще строку");
     }
 }
